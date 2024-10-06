9ce802a7f7c9c82ef57aa9841b2c7edaa28b6425
ShyGuy2712 <jgirard356@gmail.com>
2024-10-06 16:45:49 -0400

    I tried

--- src/counter-app.js
+++ src/counter-app.js
@@ -13,6 +13,8 @@ export class counterApp extends DDDSuper(LitElement) {
     this.count = 0;
     this.min= 0;
     this.max = 100;
+    this.minHit = false;
+    this.maxHit = false;
   }
 
   static get properties() {
@@ -21,6 +23,7 @@ export class counterApp extends DDDSuper(LitElement) {
       count: { type: Number },
       min: { type: Number },
       max: { type: Number },
+      fancy: {type: Boolean, reflect: true },
     };
   }
 
@@ -48,19 +51,64 @@ export class counterApp extends DDDSuper(LitElement) {
       :host([count="21"]).number {
         background-color: var(--ddd-theme-default-keystoneYellow);
       }
+      :host([fancy]) {
+        background-color: var(--ddd-theme-deafault-discoveryCoral);
+        size: 50px;
+      }
     `];
   }
 
   decrease(e) {
+    this.fancy = false;
     if (this.count > this.min) {
       this.count--;
+      if (this.count == "18") or (this.count == "21"); {
+        this.fancy = true;
+        }
+      if (this.count == this.min) {
+        this.fancy = true;
+        }
+      }
+    }
+
+  increment(e) {
+    this.fancy = false;
+    if (this.count < this.max) {
+      this.count++;
+      if (this.count == "18") or (this.count == "21"); {
+        this.fancy = true;
+        }
+      if (this.count == this.max) {
+        this.fancy = true;
+      }
     }
   }
 
-  increment(e) {
-    if (this.count < this.max) {
-      this.count++;
+  updated(changedProperties) {
+    if (changedProperties.has('counter')) {
+      // do your testing of the value and make it rain by calling makeItRain
+      this.makeItRain();
     }
+  }
+  
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
   }
 
   render() {
@@ -68,8 +116,8 @@ export class counterApp extends DDDSuper(LitElement) {
 <div class="wrapper">
   <div class="number">${this.count}</div>
   <div class="buttons">
-    <button title="decrease" @click="${this.decrease}">-</button>
-    <button title="increment" @click="${this.increment}">+</button>
+    <button title="decrease" @click="${this.decrease}" ?disabled="${this.min === this.counter}">-</button>
+    <button title="increment" @click="${this.increment}" ?disabled="${this.max === this.counter}">+</button>
   </div>
 </div>`;
   }
